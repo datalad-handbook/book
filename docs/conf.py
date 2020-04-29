6fe4ff369f249093019b417094b7c6f0e4e10d91
Adina Wagner <adina.wagner@t-online.de>
2020-04-29 09:35:24 +0200

    PDF: add a logo to make the title page slightly less crappy

--- docs/conf.py
+++ docs/conf.py
@@ -273,7 +273,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-latex_logo = None
+latex_logo = '_static/logo_small.png'
 
 latex_toplevel_sectioning = 'part'
 latex_show_pagerefs = True

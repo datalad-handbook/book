a52d2257f9b163d07615b8af0c4854bf8e67ca7b
Michael Hanke <michael.hanke@gmail.com>
2022-12-21 09:58:57 +0100

    Less noisy install-singularity

--- tools/install-singularity.sh
+++ tools/install-singularity.sh
@@ -6,6 +6,6 @@ release="$(curl -fsSL https://api.github.com/repos/sylabs/singularity/releases/l
 [ "${release}x" = "x" ] && echo "Failed to get latest singularity release name" && exit 1 || true
 codename="$(lsb_release -cs)"
 arch="$(dpkg --print-architecture)"
-wget -O /tmp/singularity-ce.deb https://github.com/sylabs/singularity/releases/download/$release/singularity-ce_${release#v}-${codename}_$arch.deb
+wget -q -O /tmp/singularity-ce.deb https://github.com/sylabs/singularity/releases/download/$release/singularity-ce_${release#v}-${codename}_$arch.deb
 sudo dpkg -i /tmp/singularity-ce.deb
-sudo apt-get install -f
+sudo apt-get install -qq -f

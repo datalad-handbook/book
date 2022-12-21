add3e073c76d91c050107d62e134487766b0e5c5
Michael Hanke <michael.hanke@gmail.com>
2022-12-21 09:44:54 +0100

    Make install-singularity fail when it should

--- tools/install-singularity.sh
+++ tools/install-singularity.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+set -e -u
+
 release="$(curl -fsSL https://api.github.com/repos/sylabs/singularity/releases/latest | jq -r .tag_name)"
+[ "${release}x" = "x" ] && echo "Failed to get latest singularity release name" && exit 1 || true
 codename="$(lsb_release -cs)"
 arch="$(dpkg --print-architecture)"
 wget -O /tmp/singularity-ce.deb https://github.com/sylabs/singularity/releases/download/$release/singularity-ce_${release#v}-${codename}_$arch.deb
